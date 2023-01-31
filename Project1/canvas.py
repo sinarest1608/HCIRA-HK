3ccaf37179ab15c8f9d11259d51c06bcceb251f4
bawejahritik <bawejahritik@gmail.com>
2023-01-31 14:47:41 -0500

    update part 3

--- Project1/canvas.py
+++ Project1/canvas.py
@@ -16,7 +16,9 @@ drawCanvas.pack(anchor='nw', expand=1, fill='both')
 
 points = []
 # To store the points while drag (Useful for next part)
-def markPoints(event):
+def mouseDown(event):
+    clearScreen()
+    print("mouseDown")
     print(event.x, event.y)
     global x, y
     x = event.x
@@ -24,24 +26,32 @@ def markPoints(event):
     
     
 # To draw line between points
-def drawLine(event):
+def mouseDrag(event):
     global x, y
     drawCanvas.create_line((x, y, event.x, event.y), fill="black")
     x = event.x
     y = event.y
+    points.append([x, y])
 
-drawCanvas.bind("<Button-1>", markPoints)
-drawCanvas.bind("<B1-Motion>", drawLine)
+def mouseUp(event):
+    print("mouse up")
+    print(event.x, event.y)
+    global x, y
+    x = event.x
+    y = event.y
+    print(points)
+
+drawCanvas.bind("<Button-1>", mouseDown)
+drawCanvas.bind("<B1-Motion>", mouseDrag)
+drawCanvas.bind("<ButtonRelease-1>", mouseUp)
 
 # part d: clearing the canvas
 def clearScreen():
     drawCanvas.delete('all')
     
+clearScreenButton = tkinter.Button(main, text = 'Clear Canvas', bd = '7', command = clearScreen)
 
-btn = tkinter.Button(main, text = 'Clear Canvas', bd = '5',
-                          command = clearScreen)
-
-btn.pack(side = 'bottom')
+clearScreenButton.pack(side = 'bottom')
 
 # print(points)
 # Put the Tkinter App in loop so it keeps running until terminated explicitly using Ctrl+C
